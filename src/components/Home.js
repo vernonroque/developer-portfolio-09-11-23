c65de2fd5d5578b75e7ab84a4385abe2d81ba81e
vernonroque <vroque88@gmail.com>
2023-10-12 11:35:11 -0500

    Installed react-type-animation and react-spring

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Home.css';
-import Typed from 'react-typed';
+// import Typed from 'react-typed';
+import { TypeAnimation } from 'react-type-animation';
 
 function Home({HomeRef}) {
   return (
@@ -8,7 +9,7 @@ function Home({HomeRef}) {
         <section className='home-container'>
             <h1 className="home-title">Welcome</h1>
             <div className = "animated-typing">
-              <Typed 
+              {/* <Typed 
                 strings = 
                 {["I'm an aspiring frontend developer",
                 "I'm a saxophonist",
@@ -17,7 +18,20 @@ function Home({HomeRef}) {
                  typeSpeed={100}
                  backSpeed={50}
                  loop
-              />
+              /> */}
+               <TypeAnimation
+                  sequence={[
+                    "I'm an aspiring frontend developer",
+                    2000,
+                    "I'm a Jazz Musician",
+                    2000,
+                    "I'm Vernon Roque",
+                    2000,
+                  ]}
+                  speed={20}
+                  repeat={Infinity}
+                  // style={{ fontSize: '2em' }}
+               />
             </div>
         </section>
     
